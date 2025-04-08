4a586bc57405803eb6cab03289b988f9cade82ee
jansredl <jan@sredl.cz>
2025-04-08 09:05:31 +0200

    Update run_all_scrapers.py

--- run_all_scrapers.py
+++ run_all_scrapers.py
@@ -1,10 +1,9 @@
-import json
 from idnes_scraper import scrape_idnes
+import json
 
 def run():
-    data = []
-    data.extend(scrape_idnes())
-
+    print("🔍 Procházím stránku 1: https://reality.idnes.cz/s/prodej/pozemky/stavebni-pozemek/cena-do-1000000/")
+    data = scrape_idnes()
     print(f"✅ Celkem inzerátů: {len(data)}")
     with open("pozemky.json", "w", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=2)
